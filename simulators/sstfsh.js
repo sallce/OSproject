4dcc730b7172bdb1debc74d5fdbcb38543a73860
Yachao Shang <yachao@coronaresearch.com.local>
2016-11-09 11:35:29 -0700

    Set up file structure

---
+++ simulators/sstfsh.js
@@ -0,0 +1,17 @@
+function Sstfsh(x, y) {//set up initialization properties
+  this.x = x;
+  this.y = y;
+
+  this.display = function() { //draw stuff
+    stroke(255);
+    fill(255, 150);
+    ellipse(this.x, this.y, 48, 48);
+  }
+
+  this.update = function() {//update properties achiving animation
+    this.x = this.x + random(-1, 1);
+    this.y = this.y + random(-1, 1);
+
+  }
+}
+
--- sketch.js
+++ sketch.js
@@ -1,11 +1,25 @@
+var colo = 255;
+var run = false;
+var sstfsh;
+var scanya;
+var cscanyi;
+var fcfsyi;
+var looksh;
+
 function setup() {
-	createCanvas(600, 400);
+	var canva = createCanvas(600, 400);
+	canva.parent("demo")
+	sstfsh = new Sstfsh(random(width),random(height));
 }
 
 function draw() {
 	background(0);
 	strokeWeight(4);
-	stroke(255);
+	stroke(colo);
+	if(run){
+
+	  sstfsh.display();
+	  sstfsh.update();
+	}
 	
-	ellipse(0, 200, 25, 25);
 }
